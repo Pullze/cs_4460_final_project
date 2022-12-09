9fda7810494404d02ccdbff85e2be32431511a8a
Lai Wang <lai@gatech.edu>
2022-12-08 22:39:35 -0500

    updated overview

--- src/Vis.js
+++ src/Vis.js
@@ -26,6 +26,11 @@ export default function Vis (props) {
       title: 'Total Victims',
       dataIndex: 'Total Victims',
       key: 'Total Victims',
+      render: (text) => (text === null ? "N/A" : text),
+      sorter: {
+        compare: (a, b) => a['Total Victims'] - b['Total Victims'],
+        multiple: 3,
+      },
     }
   ];
   return (
@@ -35,7 +40,14 @@ export default function Vis (props) {
         <Col xs={24} sm={24} md={18} lg={18} xl={19} xxl={20}>
           <Content style={{background: '#fff'}}>
             <h1> CS 4460 Final Vis</h1>
-            <Table dataSource={dataset} columns={columns} />
+            <Table dataSource={dataset}
+                   rowKey={"Case"}
+                   columns={columns}
+                   expandable={{
+                     expandedRowRender: (record) => <p style={{ margin: 0 }}>{record.summary}</p>,
+                     rowExpandable: (record) => record.summary !== null,
+                   }}
+            />
           </Content>
         </Col>
       </Row>
